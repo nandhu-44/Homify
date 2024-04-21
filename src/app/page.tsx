02a481ad6f3587d8cf948ff3156782b1a092cbc9
ab-h-i-n <abhinshelly@gmail.com>
2024-04-21 10:40:26 +0530

    Refactor page component name in about/page.tsx, add button and link in home/Home.tsx, and update README.md

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,17 @@
+"use client";
+
+import WelcomePage from '@/common/pages/welcome/Welcome';
 import HomePage from '../common/pages/home/Home'
 
 export default function Home() {
+
+  const user = localStorage.getItem('user');
+
   return (
     <main>
-      <HomePage/>
+      {
+        user ? <HomePage /> : <WelcomePage/>
+      }
     </main>
   );
 }
